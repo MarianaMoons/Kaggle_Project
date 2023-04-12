89ad3d5ca2290ea9a01d83fa77da099bf3821a20
Mariana Luna <122582598+MarianaMoons@users.noreply.github.com>
2023-04-12 15:21:17 -0600

    Initial commit

---
+++ app/s07_sql_utils_explore.py
@@ -0,0 +1,18 @@
+# Funciones para explorar las tablas existentes y su composicion
+'''
+Revisen https://github.com/sonder-art/fdd_prim_2023/blob/main/codigo/docker_compose/app/sql_tutorial.ipynb
+'''
+from .s06_sql_utils_connect import *
+
+def print_rows(table_name:str, n:int=5):
+    pass
+
+def print_metadata(table_name:str):
+    pass
+
+def print_table_columns(table_name:str, n:int=5):
+    pass
+
+def check_table_exists(table_name:str) -> bool:
+    pass
+
---
+++ app/s10_populate_tables_from_csv.py
@@ -0,0 +1,16 @@
+'''
+Funciones de limpieza de pandas, para csv. Replicar lo hecho en 02_pandas
+Pandas
+recivo datos sucios (csv) -> limpio datos y guardo en csv
+'''
+
+
+
+
+
+'''
+Funciones para subir datos limpios (con pandas) a la base de datos
+SQL alchemy. revisar https://github.com/sonder-art/fdd_prim_2023/blob/main/codigo/docker_compose/app/sql_tutorial_2.ipynb
+
+recibo datos limpios (csv) -> suben a SQLAlchemy pero con checks de integridad
+'''
